0caf9a80180a11d181754eab0785cb96e4dc0488
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-08-17 15:01:38 +0200

    some changes

--- app/models/dwarf.rb
+++ app/models/dwarf.rb
@@ -4,4 +4,5 @@ class Dwarf < ApplicationRecord
   has_many :reviews, through: :rent
   has_many_attached :photos
   validates :nickname, presence: true, uniqueness: true
+  validates :user_id, uniqueness: true
 end
