9f669848db9f762c8cce66d518ad1ee0f12125a1
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-08-17 22:36:45 +0200

    avatar fixed searchbar improved

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,11 +25,13 @@ require("channels")
 // External imports
 import "bootstrap";
 import { showNavbar } from "../components/navbar"
+import { moveSearchbar } from "../components/searchbar"
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  moveSearchbar();
   showNavbar();
 });
