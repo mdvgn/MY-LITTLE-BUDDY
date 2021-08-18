db00ba910d45c1a1ca1d1091cc4aed91f51a1c67
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-08-18 14:37:00 +0200

    skills fixed

--- app/models/dwarf.rb
+++ app/models/dwarf.rb
@@ -1,7 +1,7 @@
 class Dwarf < ApplicationRecord
   belongs_to :user
-  has_many :skills, through: :dwarf_skill
   has_many :dwarf_skills, dependent: :destroy
+  has_many :skills, through: :dwarf_skills
   has_many :reviews, through: :rent
   has_many_attached :photos
   validates :nickname, presence: true, uniqueness: true
