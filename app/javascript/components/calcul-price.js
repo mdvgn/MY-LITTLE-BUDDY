970be683ec6fedd7f9cfea7ef6b2ae106da6e107
Sebastien Rousseau <sebounoob@gmail.com>
2021-08-20 15:52:31 +0200

    js

--- app/javascript/components/calcul-price.js
+++ app/javascript/components/calcul-price.js
@@ -1,9 +1,9 @@
 const priceCalculator = () => {
   const rentStart = document.querySelector("#rent_start_date")
   const rentEnd = document.querySelector("#rent_end_date")
-  const pricePerHour = parseInt(document.querySelector('#price').dataset.pricePerHour, 10)
-  const prout = document.querySelector("#prout")
   if (rentStart) {
+    const prout = document.querySelector("#prout")
+    const pricePerHour = parseInt(document.querySelector('#price').dataset.pricePerHour, 10)
     rentEnd.addEventListener('change', (event) => {
       const startDateTime = new Date(rentStart.value).getTime() / (1000 * 60);
       const endDateTime = new Date(rentEnd.value).getTime() / (1000 * 60);
